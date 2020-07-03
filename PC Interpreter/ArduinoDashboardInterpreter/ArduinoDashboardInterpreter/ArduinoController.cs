8bf7015cfd22c715a279d4bc04bfc4e99d24f95c
Daniel Alberski <danielalberski@gmail.com>
2020-07-03 15:48:40 +0200

    basic code

---
+++ PC Interpreter/ArduinoDashboardInterpreter/ArduinoDashboardInterpreter/ArduinoController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ArduinoDashboardInterpreter
+{
+    class ArduinoController
+    {
+        LedState[] LedController = new LedState[16];
+        public delegate void LedStateChangedDelegate(LedType id, LedState state);
+        public event LedStateChangedDelegate LedStateChanged;
+
+        LedState[] BacklightController = new LedState[5];
+        public delegate void BacklightStateChangedDelegate(BacklightType id, LedState state);
+        public event BacklightStateChangedDelegate BacklightStateChanged;
+
+        Double[] GaugePositions = new double[4];
+        public delegate void GaugePositionChangedDelegate(GaugeType id, Double value);
+        public event GaugePositionChangedDelegate GaugePositionChanged;
+
+        ProgramType CurrentProgram = ProgramType.Manual;
+        public delegate void ProgramChangedDelegate(ProgramType newProgram);
+        public event ProgramChangedDelegate ProgramChanged;
+
+        #region "LED"
+
+        public enum LedType
+        {
+            CheckEngine,
+            GlowPlug,
+            LowBeam,
+            HighBeam,
+            Fuel,
+            LiftAxle,
+            DiffLock,
+            LeftIndicator,
+            RightIndicator,
+            CruiseControl,
+            Rest,
+            WarningBrake,
+            ParkingBrake,
+            Battery,
+            Oil,
+            Retarder
+        }
+
+        public enum BacklightType
+        {
+            WhiteBig,
+            WhiteSmall,
+            RedBig,
+            RedSmall,
+            LcdLed
+        }
+
+        public enum LedState
+        {
+            Off,
+            On,
+            Blink
+        }
+
+        public static Brush GetLedColor(LedType id)
+        {
+            switch(id)
+            {
+                case LedType.LowBeam:
+                case LedType.LeftIndicator:
+                case LedType.RightIndicator:
+                case LedType.CruiseControl:
+                case LedType.Retarder:
+                    return Brushes.LightGreen;
+                case LedType.CheckEngine:
+                case LedType.GlowPlug:
+                case LedType.Fuel:
+                case LedType.LiftAxle:
+                case LedType.DiffLock:
+                    return Brushes.Yellow;
+                case LedType.Rest:
+                case LedType.WarningBrake:
+                case LedType.ParkingBrake:
+                case LedType.Battery:
+                case LedType.Oil:
+                    return Brushes.Red;
+                case LedType.HighBeam:
+                    return Brushes.DodgerBlue;
+                default:
+                    return Brushes.Pink;
+            }
+        }
+
+        public LedState GetLedState(LedType id)
+        {
+            return LedController[(int)id];
+        }
+
+        public void SetLedState(LedType id, LedState state)
+        {
+            LedState currentState = LedController[(int)id];
+            LedController[(int)id] = state;
+            if (currentState != state) LedStateChanged?.Invoke(id, state);
+        }
+        #endregion
+
+        #region "BACKLIGHT"
+
+        public LedState GetBacklightState(BacklightType id)
+        {
+            return BacklightController[(int)id];
+        }
+
+        public void SetBacklightState(BacklightType id, LedState state)
+        {
+            LedState currentState = BacklightController[(int)id];
+            BacklightController[(int)id] = state;
+            if (currentState != state) BacklightStateChanged?.Invoke(id, state);
+        }
+        #endregion
+
+        #region "GAUGE"
+
+        public enum GaugeType
+        {
+            Speed,
+            Fuel,
+            Air,
+            Engine
+        }
+
+        public Double GetGaugePosition(GaugeType id)
+        {
+            return GaugePositions[(int)id];
+        }
+
+        public void SetGaugePosition(GaugeType id, Double value)
+        {
+            Double currentValue = GaugePositions[(int)id];
+            if (value < 0) value = 0;
+            if (value > 100) value = 100;
+            GaugePositions[(int)id] = value;
+            if (currentValue != value) GaugePositionChanged?.Invoke(id, value);
+        }
+        #endregion
+
+        #region "PROGRAM"
+
+        public enum ProgramType
+        {
+            Home,
+            Manual,
+            Test,
+            Telemetry
+        }
+
+        public ProgramType GetCurrentProgram()
+        {
+            return CurrentProgram;
+        }
+
+        public void SetNewProgram(ProgramType newProgram)
+        {
+            ProgramType oldProgram = CurrentProgram;
+            CurrentProgram = newProgram;
+            if (oldProgram != CurrentProgram) ProgramChanged?.Invoke(newProgram);
+        }
+        #endregion
+    }
+}
--- PC Interpreter/ArduinoDashboardInterpreter/ArduinoDashboardInterpreter/MainWindow.xaml.cs
+++ PC Interpreter/ArduinoDashboardInterpreter/ArduinoDashboardInterpreter/MainWindow.xaml.cs
@@ -20,9 +20,183 @@ namespace ArduinoDashboardInterpreter
     /// </summary>
     public partial class MainWindow : Window
     {
+        ArduinoController arduino;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            arduino = new ArduinoController();
+            arduino.LedStateChanged += LedButtonColor;
+            arduino.BacklightStateChanged += BacklightButtonColor;
+            arduino.GaugePositionChanged += GaugeSliderUpdate;
+            arduino.ProgramChanged += ProgramButtonColor;
         }
+
+        #region "LED"
+
+        private void LedButtonColor(ArduinoController.LedType id, ArduinoController.LedState state)
+        {
+            Brush color = state == ArduinoController.LedState.Off ? Brushes.White : ArduinoController.GetLedColor(id);
+            switch ((int)id)
+            {
+                case 0: Led0.Background = color; break;
+                case 1: Led1.Background = color; break;
+                case 2: Led2.Background = color; break;
+                case 3: Led3.Background = color; break;
+                case 4: Led4.Background = color; break;
+                case 5: Led5.Background = color; break;
+                case 6: Led6.Background = color; break;
+                case 7: Led7.Background = color; break;
+                case 8: Led8.Background = color; break;
+                case 9: Led9.Background = color; break;
+                case 10: Led10.Background = color; break;
+                case 11: Led11.Background = color; break;
+                case 12: Led12.Background = color; break;
+                case 13: Led13.Background = color; break;
+                case 14: Led14.Background = color; break;
+                case 15: Led15.Background = color; break;
+            }
+        }
+
+        private void LedButton_Click(object sender, RoutedEventArgs e)
+        {
+            ArduinoController.LedType id = (ArduinoController.LedType)int.Parse(((Button)sender).Name.Substring(3));
+            ArduinoController.LedState state = arduino.GetLedState(id);
+            if (state == ArduinoController.LedState.Off)
+            {
+                state = ArduinoController.LedState.On;
+            }
+            else
+            {
+                state = ArduinoController.LedState.Off;
+            }
+            arduino.SetLedState(id, state);
+        }
+        #endregion
+
+        #region "BACKLIGHT"
+
+        private void BacklightButtonColor(ArduinoController.BacklightType id, ArduinoController.LedState state)
+        {
+            Brush color = state == ArduinoController.LedState.Off ? Brushes.White : Brushes.LightBlue;
+            switch (id)
+            {
+                case ArduinoController.BacklightType.WhiteBig: BacklightWB.Background = color; break;
+                case ArduinoController.BacklightType.WhiteSmall: BacklightWS.Background = color; break;
+                case ArduinoController.BacklightType.RedBig: BacklightRB.Background = color; break;
+                case ArduinoController.BacklightType.RedSmall: BacklightRS.Background = color; break;
+                case ArduinoController.BacklightType.LcdLed: LcdLed.Background = color; break;
+            }
+        }
+
+        private void BacklightButton_Click(object sender, RoutedEventArgs e)
+        {
+            ArduinoController.BacklightType id = 0;
+            switch (((Button)sender).Name)
+            {
+                case "BacklightWB": id = ArduinoController.BacklightType.WhiteBig; break;
+                case "BacklightWS": id = ArduinoController.BacklightType.WhiteSmall; break;
+                case "BacklightRB": id = ArduinoController.BacklightType.RedBig; break;
+                case "BacklightRS": id = ArduinoController.BacklightType.RedSmall; break;
+                case "LcdLed": id = ArduinoController.BacklightType.LcdLed; break;
+            }
+            ArduinoController.LedState state = arduino.GetBacklightState(id);
+            if (state == ArduinoController.LedState.Off)
+            {
+                state = ArduinoController.LedState.On;
+            }
+            else
+            {
+                state = ArduinoController.LedState.Off;
+            }
+            arduino.SetBacklightState(id, state);
+        }
+        #endregion
+
+        #region "GAUGE"
+
+        bool ignoreSliderUpdate = false;
+
+        private void Gauge_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(!ignoreSliderUpdate)
+            {
+                ArduinoController.GaugeType id = 0;
+                switch (((Slider)sender).Name.Substring(5))
+                {
+                    case "A": id = ArduinoController.GaugeType.Speed; break;
+                    case "B": id = ArduinoController.GaugeType.Fuel; break;
+                    case "C": id = ArduinoController.GaugeType.Air; break;
+                    case "D": id = ArduinoController.GaugeType.Engine; break;
+                }
+                arduino.SetGaugePosition(id, e.NewValue);
+            }
+        }
+
+        private void GaugeSliderUpdate(ArduinoController.GaugeType id, double value)
+        {
+            ignoreSliderUpdate = true;
+            switch (id)
+            {
+                case ArduinoController.GaugeType.Speed: GaugeA.Value = value; break;
+                case ArduinoController.GaugeType.Fuel: GaugeB.Value = value; break;
+                case ArduinoController.GaugeType.Air: GaugeC.Value = value; break;
+                case ArduinoController.GaugeType.Engine: GaugeD.Value = value; break;
+            }
+            ignoreSliderUpdate = false;
+        }
+
+        private void GaugeHome_Click(object sender, RoutedEventArgs e)
+        {
+            ignoreSliderUpdate = true;
+            arduino.SetGaugePosition(ArduinoController.GaugeType.Speed, 0);
+            arduino.SetGaugePosition(ArduinoController.GaugeType.Fuel, 0);
+            arduino.SetGaugePosition(ArduinoController.GaugeType.Air, 0);
+            arduino.SetGaugePosition(ArduinoController.GaugeType.Engine, 0);
+            ignoreSliderUpdate = false;
+        }
+
+        private void GaugeMax_Click(object sender, RoutedEventArgs e)
+        {
+            ignoreSliderUpdate = true;
+            arduino.SetGaugePosition(ArduinoController.GaugeType.Speed, 100);
+            arduino.SetGaugePosition(ArduinoController.GaugeType.Fuel, 100);
+            arduino.SetGaugePosition(ArduinoController.GaugeType.Air, 100);
+            arduino.SetGaugePosition(ArduinoController.GaugeType.Engine, 100);
+            ignoreSliderUpdate = false;
+        }
+        #endregion
+
+        #region "PROGRAM"
+
+        private void ProgramButtonColor(ArduinoController.ProgramType newProgram)
+        {
+            Brush offColor = Brushes.White;
+            Brush onColor = Brushes.LightBlue;
+            ProgHome.Background = offColor;
+            ProgManual.Background = offColor;
+            ProgTest.Background = offColor;
+            ProgTelemetry.Background = offColor;
+            switch (newProgram)
+            {
+                case ArduinoController.ProgramType.Home: ProgHome.Background = onColor; break;
+                case ArduinoController.ProgramType.Manual: ProgManual.Background = onColor; break;
+                case ArduinoController.ProgramType.Test: ProgTest.Background = onColor; break;
+                case ArduinoController.ProgramType.Telemetry: ProgTelemetry.Background = onColor; break;
+            }
+        }
+
+        private void ProgramButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch(((Button)sender).Name)
+            {
+                case "ProgHome": arduino.SetNewProgram(ArduinoController.ProgramType.Home); break;
+                case "ProgManual": arduino.SetNewProgram(ArduinoController.ProgramType.Manual); break;
+                case "ProgTest": arduino.SetNewProgram(ArduinoController.ProgramType.Test); break;
+                case "ProgTelemetry": arduino.SetNewProgram(ArduinoController.ProgramType.Telemetry); break;
+            }
+        }
+        #endregion
     }
 }
