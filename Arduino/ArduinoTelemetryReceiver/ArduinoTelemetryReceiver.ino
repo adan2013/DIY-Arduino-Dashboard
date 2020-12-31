776adfccbcf2a6c52947f51409d17c9e312a4138
Daniel Alberski <danielalberski@gmail.com>
2020-12-31 17:59:23 +0100

    test animation

--- Arduino/ArduinoTelemetryReceiver/ArduinoTelemetryReceiver.ino
+++ Arduino/ArduinoTelemetryReceiver/ArduinoTelemetryReceiver.ino
@@ -2,8 +2,8 @@
 #include <Wire.h>
 #include <Adafruit_MCP23017.h>
 
-#define BACKLIGHT_POTENTIOMETER 15
-#define BACKLIGHT_MIN_DUTY 30
+#define BACKLIGHT_POTENTIOMETER A8
+#define BACKLIGHT_MIN_DUTY 10
 #define BACKLIGHT_MAX_DUTY 128
 
 #define BACKLIGHT_WB 12
@@ -14,43 +14,19 @@
 
 #define MOTOR_STEPS 720
 #define MOTOR_SPEED 30
-#define MOTOR_A_1 42
-#define MOTOR_B_1 38
-#define MOTOR_C_1 34
-#define MOTOR_D_1 30
+#define MOTOR_A_1 38
+#define MOTOR_B_1 34
+#define MOTOR_C_1 30
+#define MOTOR_D_1 26
 
-Adafruit_MCP23017 LedController;
-Stepper GaugeMotorA(MOTOR_STEPS, MOTOR_A_1, MOTOR_A_1 + 1, MOTOR_A_1 + 2, MOTOR_A_1 + 3);
-Stepper GaugeMotorB(MOTOR_STEPS, MOTOR_B_1, MOTOR_B_1 + 1, MOTOR_B_1 + 2, MOTOR_B_1 + 3);
-Stepper GaugeMotorC(MOTOR_STEPS, MOTOR_C_1, MOTOR_C_1 + 1, MOTOR_C_1 + 2, MOTOR_C_1 + 3);
-Stepper GaugeMotorD(MOTOR_STEPS, MOTOR_D_1, MOTOR_D_1 + 1, MOTOR_D_1 + 2, MOTOR_D_1 + 3);
-
-void setup() {
-  Serial.begin(9600);
-  //LED CONTROLLER
-  LedController.begin();
-  for(int i = 0; i < 16; i++) LedController.pinMode(i, OUTPUT);
-  updateLed("0000000000000000");
-  //BACKLIGHT
-  pinMode(BACKLIGHT_WB, OUTPUT);
-  pinMode(BACKLIGHT_WS, OUTPUT);
-  pinMode(BACKLIGHT_RB, OUTPUT);
-  pinMode(BACKLIGHT_RS, OUTPUT);
-  pinMode(BACKLIGHT_LCD, OUTPUT);
-  updateBacklight("00000");
-  //GAUGE
-  GaugeMotorA.setSpeed(MOTOR_SPEED);
-  GaugeMotorB.setSpeed(MOTOR_SPEED);
-  GaugeMotorC.setSpeed(MOTOR_SPEED);
-  GaugeMotorD.setSpeed(MOTOR_SPEED);
-}
-
-void loop() {
-  
-}
+Adafruit_MCP23017 ledController;
+Stepper gaugeMotorA(MOTOR_STEPS, MOTOR_A_1, MOTOR_A_1 + 1, MOTOR_A_1 + 2, MOTOR_A_1 + 3);
+Stepper gaugeMotorB(MOTOR_STEPS, MOTOR_B_1, MOTOR_B_1 + 1, MOTOR_B_1 + 2, MOTOR_B_1 + 3);
+Stepper gaugeMotorC(MOTOR_STEPS, MOTOR_C_1, MOTOR_C_1 + 1, MOTOR_C_1 + 2, MOTOR_C_1 + 3);
+Stepper gaugeMotorD(MOTOR_STEPS, MOTOR_D_1, MOTOR_D_1 + 1, MOTOR_D_1 + 2, MOTOR_D_1 + 3);
 
 void updateLed(String state) {
-  for(int i = 0; i < 16; i++) LedController.digitalWrite(i, state.substring(i, i+1) == "1" ? HIGH : LOW);
+  for(int i = 0; i < 16; i++) ledController.digitalWrite(i, state.substring(i, i+1) == "1" ? HIGH : LOW);
 }
 
 void updateBacklight(String state){
@@ -60,4 +36,54 @@ void updateBacklight(String state){
   digitalWrite(BACKLIGHT_RB, state.substring(2, 3) == "1" ? HIGH : LOW);
   digitalWrite(BACKLIGHT_RS, state.substring(3, 4) == "1" ? HIGH : LOW);
   digitalWrite(BACKLIGHT_LCD, state.substring(4) == "0" ? HIGH : LOW);
+}
+
+void setup() {
+  Serial.begin(9600);
+  //LED CONTROLLER
+  ledController.begin();
+  for(int i = 0; i < 16; i++) ledController.pinMode(i, OUTPUT);
+  updateLed("0000000000000000");
+  //BACKLIGHT
+  pinMode(BACKLIGHT_WB, OUTPUT);
+  pinMode(BACKLIGHT_WS, OUTPUT);
+  pinMode(BACKLIGHT_RB, OUTPUT);
+  pinMode(BACKLIGHT_RS, OUTPUT);
+  pinMode(BACKLIGHT_LCD, OUTPUT);
+  updateBacklight("00000");
+  //GAUGE
+  gaugeMotorA.setSpeed(MOTOR_SPEED);
+  gaugeMotorB.setSpeed(MOTOR_SPEED);
+  gaugeMotorC.setSpeed(MOTOR_SPEED);
+  gaugeMotorD.setSpeed(MOTOR_SPEED);
+}
+
+void loop() {
+  String setup = "";
+  for(int ledAnim = 0; ledAnim < 16; ledAnim++) {
+    setup = "";
+    for(int i = 0; i < 16; i++) {
+      if(i == ledAnim || i == (15 - ledAnim)) {
+        setup += "1";
+      }else{
+        setup += "0";
+      }
+    }
+    updateLed(setup);
+    delay(200);
+  }
+  updateLed("0000000000000000");
+  delay(1000);
+  updateBacklight("10000");
+  delay(1000);
+  updateBacklight("11000");
+  delay(1000);
+  updateBacklight("11100");
+  delay(1000);
+  updateBacklight("11110");
+  delay(1000);
+  updateBacklight("11111");
+  delay(1000);
+  updateBacklight("00000");
+  delay(1000);
 }
